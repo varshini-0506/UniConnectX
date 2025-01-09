7fa387815e03e89ff60c08a54415b9b34325aee6
Varshini <varshiniprabakaran0506@gmail.com>
2025-01-09 09:40:04 +0530

    added initial files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+//import React from "react";
+import ReactDOM from "react-dom";
+
+const App=()=>{
+    return (
+        <div>App</div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
